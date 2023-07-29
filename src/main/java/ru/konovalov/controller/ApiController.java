f2e1e6f5ff3ec0173bf988d75e1ee3c3b120ef4a
Андрей Коновалов <akonovalov2015@mail.ru>
2023-07-29 18:16:43 +0500

    Added dependencies, config and controller.

---
+++ src/main/java/ru/konovalov/controller/ApiController.java
@@ -0,0 +1,13 @@
+package ru.konovalov.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping
+    public String main(){
+        return "Hello, World";
+    }
+}
