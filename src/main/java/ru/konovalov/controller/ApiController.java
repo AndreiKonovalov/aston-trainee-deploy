c41639279e16ded16259895fbb66c80175a686b9
Андрей Коновалов <akonovalov2015@mail.ru>
2023-07-30 17:14:09 +0500

    test №2 commit

--- src/main/java/ru/konovalov/controller/ApiController.java
+++ src/main/java/ru/konovalov/controller/ApiController.java
@@ -1,13 +1,15 @@
 package ru.konovalov.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/hello")
 @RestController
 public class ApiController {
 
     @GetMapping
     public String main(){
-        return "Hello, Jenkins";
+        return "Hello, Tomcat";
     }
 }
