452073dc25ea820f963c8874a5ff2a818d843331
Андрей Коновалов <akonovalov2015@mail.ru>
2023-07-30 20:47:52 +0500

    test №3 commit

--- src/main/java/ru/konovalov/controller/ApiController.java
+++ src/main/java/ru/konovalov/controller/ApiController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/hello")
+@RequestMapping("/hello")
 @RestController
 public class ApiController {
 
